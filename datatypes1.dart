270651ff56ee046090e449736dc554230d73b45b
mwangikigoro <160245406+mwangikigoro@users.noreply.github.com>
2024-07-03 12:58:21 +0300

    Add files via upload

---
+++ datatypes1.dart
@@ -0,0 +1,25 @@
+void main() {
+  // Example of int data type
+  int age = 30;
+  print("Age: $age");
+
+  // Example of double data type
+  double salary = 50000.75;
+  print("Salary: \$salary");
+
+  // Example of String data type
+  String name = "Mwangi Kigoro";
+  print("Name: $name");
+
+  // Example of List data type (List of integers)
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print("Numbers: $numbers");
+
+  // Example of Map data type (Key-value pairs)
+  Map<String, dynamic> personInfo = {
+    'name': 'Irene',
+    'age': 25,
+    'isStudent': true,
+  };
+  print("Person Info: $personInfo");
+}
